6ae4d5260586f6ae1d13cb6aef29d39015cfcc78
Kevin Li <kevinli4321@gmail.com>
2020-10-17 22:21:26 -0400

    Homework setup

---
+++ src/applications/arithmetic/DyckWord.java
@@ -0,0 +1,25 @@
+package applications.arithmetic;
+
+/**
+ * @author Ritwik Banerjee
+ */
+public class DyckWord {
+
+    private final String word;
+
+    public DyckWord(String word) {
+        if (isDyckWord(word))
+            this.word = word;
+        else
+            throw new IllegalArgumentException(String.format("%s is not a valid Dyck word.", word));
+    }
+
+    private static boolean isDyckWord(String word) {
+        // todo
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+}
