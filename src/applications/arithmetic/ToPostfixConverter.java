767d17954573c02205d696fc1f5f390846dd23d6
Kevin Li <kevinli4321@gmail.com>
2020-10-19 17:07:41 -0400

    nextToken and isOperand implemented

--- src/applications/arithmetic/ToPostfixConverter.java
+++ src/applications/arithmetic/ToPostfixConverter.java
@@ -1,15 +1,30 @@
 package applications.arithmetic;
 
+import datastructures.sequential.Stack;
+
 public class ToPostfixConverter implements Converter {
     public String convert(ArithmeticExpression expression) {
-        return "";
+        return nextToken("1+2+3+45+83151.0+10", 6);
     }
 
+    // This function assumes whatever uses it will call it only at the start of a token.
     public String nextToken(String s, int start) {
-        return "";
+        String charAtIdx = s.substring(start, start + 1);
+        if (!isOperand(charAtIdx)) return charAtIdx; // operators can be 1 character long only so just return
+
+        TokenBuilder token = new TokenBuilder(); // is a number, might be more than 1 character long
+        while (start < s.length() && isOperand(charAtIdx)) {
+            token.append(charAtIdx.charAt(0)); // 1 character string anyway
+            start++;
+            charAtIdx = s.substring(start, start + 1); // set character to next in the string
+        }
+
+        return token.build();
     }
 
     public boolean isOperand(String s) {
-        return true;
+        return !Operator.isOperator(s) &&
+               !Brackets.isLeftBracket(s) &&
+               !Brackets.isRightBracket(s);
     }
 }
